462bfe426292fc70e34469775c5f696d2cca0358
Jan Razny <netorg@gmx.de>
2022-07-27 23:25:25 +0200

    addTodo / firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { collectionData, Firestore, collection, initializeFirestore, doc } from '@angular/fire/firestore';
+import { setDoc } from '@firebase/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'firebase-todolist';
+  todos$: Observable<any>;
+  todos: Array<any>;
+  todoText: string = '';
+
+  constructor(private firestore: Firestore) {
+    const coll = collection(firestore, 'todos');
+    this.todos$ = collectionData(coll);
+
+    this.todos$.subscribe( (newTodoes) => {
+      console.log('Neue Todoes sind: ', newTodoes);
+      this.todos = newTodoes;
+      // Ton abspielen oder Nachricht anzeigen etc.
+    });
+
+      // oder diese Variante - obere Var ist besser. da mehr Möglichkeiten - z.B. Ton abspielen oder Nachricht anzeigen
+      // export class AppComponent {
+      //   todos$: Observable<any>;
+
+      //   constructor(firestore: Firestore) {
+      //     const coll = collection(firestore, 'todos');
+      //     this.todos$ = collectionData(coll);
+      //   }
+      // }
+
+  }
+
+  addTodo() {
+    const coll = collection(this.firestore, 'todos');
+    setDoc(doc(coll), {name: this.todoText});
+  }
 }
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'firebase-todolist-jra',
+    appId: '1:23720648910:web:e0a5a1438e0496598ff09f',
+    storageBucket: 'firebase-todolist-jra.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAp8t4TYY879tiyRp88cmIUGWlNqun_UWU',
+    authDomain: 'fir-todolist-jra.firebaseapp.com',
+    messagingSenderId: '23720648910',
+    measurementId: 'G-3ZEJWB0E1N',
+  },
   production: false
 };
 
